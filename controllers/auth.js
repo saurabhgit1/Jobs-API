974afd9a1a8694ac09a587deb8047d23e4aa8276
saurabhcs1 <singhsaurabhkumar84@gmail.com>
2024-02-03 23:06:11 +0530

    password hashing moved to mongoose mw from controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,13 +10,15 @@ const register = async (req, res, next) => {
     // if (!email || !name || !password) {
     //   throw new BadRequestErrors("Please provid email, name and password.");
     // }
+
+    // below hashing of password should be moved to middleware (at mongoose schema)
     const { email, name, password } = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(password, salt);
 
     // const user = await User.create({ ...req.body });
-    const user = await User.create({ email, name, password: hashedPassword });
+    const user = await User.create({ email, name, password });
 
     res.status(StatusCodes.CREATED).json({ user });
   } catch (error) {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -21,6 +21,11 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minlength: 6,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", UserSchema);
