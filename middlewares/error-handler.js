97c64759810d57af3efa8884917caa95954c29ae
saurabhcs1 <singhsaurabhkumar84@gmail.com>
2024-02-07 23:10:03 +0530

    duplicate error + validaton error handled in error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,10 +1,27 @@
 import CustomErrors from "../errors/custom-errors.js";
 import { StatusCodes } from "http-status-codes";
 const errorHandler = (error, req, res, next) => {
-  if (error instanceof CustomErrors) {
-    return res.status(error.statusCode).json({ message: error.message });
+  const customError = {
+    statusCode: error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: error.message || "Something Went Wrong!!!",
+  };
+  // if (error instanceof CustomErrors) {
+  //   return res.status(error.statusCode).json({ message: error.message });
+  // }
+  if (error.code && error.code === 11000) {
+    customError.msg =
+      "Duplicate value entered for " +
+      Object.keys(error.keyValue) +
+      " Please choose another value.";
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error });
+  if (error.name === "ValidatonError") {
+    customError.msg = Object.values(error.errors)
+      .map((item) => item.messgage)
+      .join(", ");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 export default errorHandler;
