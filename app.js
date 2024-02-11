a8a007d6dcc69b74aac73bb1d04e20f071ec81ea
saurabhcs1 <singhsaurabhkumar84@gmail.com>
2024-02-11 20:43:28 +0530

    security pkgs added

--- app.js
+++ app.js
@@ -8,13 +8,25 @@ import routeNotFoundMW from "./middlewares/route-not-found.js";
 import errorHandlerMW from "./middlewares/error-handler.js";
 import connectToDB from "./db/connectdb.js";
 import checkAuth from "./middlewares/auth.js";
+
+//extra security pkgs
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss-clean";
+import rateLimit from "express-rate-limit";
+
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }));
 
 app.use(express.json());
 
 //routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/jobs", checkAuth,jobsRoutes);
+app.use("/api/v1/jobs", checkAuth, jobsRoutes);
 
 app.use(routeNotFoundMW);
 app.use(errorHandlerMW);
